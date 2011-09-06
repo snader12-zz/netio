b2adccc5f726901923ab32e083e7c049f94c26b3
Sadullah Nader <snader12@Sadullah-Naders-MacBook-Pro.local>
2011-09-05 20:11:40 -0700

    Initial Commit

---
+++ Netio/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Netio
+//
+//  Created by Sadullah Nader on 9/5/11.
+//  Copyright 2011 F9E. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
