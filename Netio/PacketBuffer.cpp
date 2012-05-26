4000be0e605f4b6fece020691dfb6b20d9005933
Saad Nader <snader12@gmail.com>
2012-05-26 15:21:15 -0700

    Updated packet buffer with new interfaces

--- Netio/PacketBuffer.cpp
+++ Netio/PacketBuffer.cpp
@@ -23,5 +23,22 @@ PacketBuffer::~PacketBuffer()
 	m_size = 0;
 	m_capacity = 0;
 }
+	
+PacketBuffer::ReadResult PacketBuffer::readBits(char *byteArray, size_t bitsToRead)
+{
+	if (m_size == 0)
+		return ReadResult_Empty;
+	if (m_size < bitsToRead)
+		memcpy(byteArray, m_buffer, m_size);
+	else
+		memcpy(byteArray, m_buffer, bitsToRead);
+}
+	
+PacketBuffer::WriteResult PacketBuffer::writeBits(char *byteArray, size_t bitsToWrite)
+{
+	if (m_size >= m_capacity)
+		return WriteResult_Full;
+	else if (
+}
 
 }
--- Netio/PacketBuffer.h
+++ Netio/PacketBuffer.h
@@ -18,14 +18,36 @@ namespace netio
 class PacketBuffer
 {
 public:
+	enum ReadResult
+	{
+		ReadResult_Empty,
+		ReadResult_Partial,
+		ReadResult_Full
+	};
+	
+	enum WriteResult
+	{
+		WriteResult_Empty,
+		WriteResult_Partial,
+		WriteResult_Full
+	};
+	
 	PacketBuffer();
 	PacketBuffer(size_t bufferCapacity);
 	~PacketBuffer();
 	size_t getCapacity() { return m_capacity; }
 	size_t getSize() { return m_size; }
 	const void * getRawBuffer() { return m_buffer; }
-	void readBits(char *byteArray, size_t bitsToRead);
-	void writeBits(char *byteArray, size_t bitsToWrite);
+	
+	ReadResult readBits(char *byteArray, size_t bitsToRead);
+	float readFloat(size_t bitsToRead);
+	int readInt(size_t bitsToRead);
+	double readDouble(size_t bitsToRead);
+	
+	WriteResult writeBits(char *byteArray, size_t bitsToWrite);
+	WriteResult writeFloat(float number, size_t bitsToWrite);
+	WriteResult writeInt(int number, size_t bitsToWrite);
+	WriteResult writeDouble(double number, size_t bitsToWrite);
 private:
 	size_t m_capacity;
 	size_t m_size;
