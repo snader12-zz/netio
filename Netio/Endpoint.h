bec01e4125d5ce012f39bc679a658071a526f7fa
Saad Nader <snader12@gmail.com>
2014-08-03 14:48:30 -0700

    xcode crap

--- Netio/Endpoint.h
+++ Netio/Endpoint.h
@@ -18,15 +18,23 @@ class Endpoint
 public:
     // addressString should be formatted as a dotted ip address, e.g. "X.X.X.X"
 	// port can be as is, i.e. port 12345.
-	Endpoint(std::string addressString, unsigned short port)
+	Endpoint(std::string addressString, uint16_t port)
 		: m_addressString(addressString), m_port(port)
 	{
+	}
+	
+	Endpoint(uint32_t ipAddress, uint16_t port)
+		: m_addressString(), m_port(port)
+	{
+		
 	}
 	
 	Endpoint(const Endpoint &rhs)
 		: m_addressString(rhs.m_addressString), m_port(rhs.m_port)
 	{
 	}
+	
+	
 	const std::string & getAddress() { return m_addressString; }
     unsigned short getPort() { return m_port; }
 private:
